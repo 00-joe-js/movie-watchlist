9b8155fdd62c78d6bf788be7facd86ae416d59b5
Joe Alves <joseph.michael.alves@gmail.com>
2021-09-30 18:18:08 -0400

    Take 7, filtering by watched and genre

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -7,13 +7,49 @@ const { Movie, Genre } = require("../db");
 
 // GET /movies
 router.get("/", async (req, res, next) => {
+    const onlyUnwatched = req.query.unwatched === "1";
+    const genreName = req.query.genre;
+
+    const whereClause = {};
+
+    if (onlyUnwatched === true) {
+        whereClause.watched = false;
+    }
+
     try {
-        const movies = await Movie.findAll({
-            include: [Genre],
-            order: [
-                ["title", "ASC"]
-            ]
-        });
+
+        let movies;
+        if (genreName) {
+
+            const specificGenre = await Genre.findOne({
+                where: {
+                    name: genreName
+                }
+            });
+
+            if (!specificGenre) {
+                res.status(404).send("Unknown genre");
+                return;
+            }
+
+            movies = await specificGenre.getMovies({
+                include: [Genre],
+                order: [
+                    ["title", "ASC"]
+                ],
+                where: whereClause
+            });
+
+        } else {
+            movies = await Movie.findAll({
+                include: [Genre],
+                order: [
+                    ["title", "ASC"]
+                ],
+                where: whereClause
+            });
+        }
+        
         res.send(
             `
                 <!DOCTYPE html>
@@ -24,6 +60,10 @@ router.get("/", async (req, res, next) => {
                     </head>
                     <body>
                         <h1>Movie List</h1>
+                        <nav>
+                            <a href="/movies?unwatched=1">Only Unwatched</a>
+
+                        </nav>
                         <ul>
                             ${movies.map((movie) => {
                                 return `
@@ -32,7 +72,7 @@ router.get("/", async (req, res, next) => {
                                         ${movie.imdbLink ? `<a target="_blank" href="${movie.imdbLink}">IMDB</a>` : ""}
                                         <ul>
                                             ${movie.genres.map(genre => {
-                                                return `<li>${genre.name}</li>`;
+                                                return `<li><a href="/movies?genre=${genre.name}">${genre.name}</a></li>`;
                                             }).join("")}
                                         </ul>
                                         ${movie.watched === false ? `<a href="/movies/${movie.id}/mark-watched">I watched this!</a>` : ""}
@@ -102,6 +142,7 @@ router.get("/:movieId/mark-watched", async (req, res, next) => {
 
         if (!theMovie) {
             res.status(404).send("No movie with that id");
+            return;
         }
 
         theMovie.watched = true;
