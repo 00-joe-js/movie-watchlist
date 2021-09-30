95d1885caf7f4c7f014bd056567b2e09ff60b3f9
Joe Alves <joseph.michael.alves@gmail.com>
2021-09-30 17:47:39 -0400

    Take 5, movie list

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -6,8 +6,44 @@ module.exports = router;
 const { Movie, Genre } = require("../db");
 
 // GET /movies
-router.get("/", (req, res) => {
-    res.send("Movie list haha");
+router.get("/", async (req, res, next) => {
+    try {
+        const movies = await Movie.findAll({
+            include: [Genre],
+            order: [
+                ["title", "ASC"]
+            ]
+        });
+        res.send(
+            `
+                <!DOCTYPE html>
+                <html>
+                    <head><title>Movie List</title></head>
+                    <body>
+                        <h1>Movie List</h1>
+                        <ul>
+                            ${movies.map((movie) => {
+                                return `
+                                    <li>
+                                        <h2>${movie.title}</h2>
+                                        ${movie.imdbLink ? `<a target="_blank" href="${movie.imdbLink}">IMDB</a>` : ""}
+                                        <ul>
+                                            ${movie.genres.map(genre => {
+                                                return `<li>${genre.name}</li>`;
+                                            }).join("")}
+                                        </ul>
+                                    </li>
+                                `
+                            }).join("")}
+                        </ul>
+                    </body>
+                </html>
+            `
+        );
+    } catch (e) {
+        next(e);
+    }
+   
 });
 
 // GET /movies/add-movie
